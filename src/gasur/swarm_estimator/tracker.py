5f296cfa20c1b8b310284363702c733da8218d24
Vince <vwhill@crimson.ua.edu>
2020-10-30 15:53:30 -0700

    Defined PHD filter classes

--- src/gasur/swarm_estimator/tracker.py
+++ src/gasur/swarm_estimator/tracker.py
@@ -65,7 +65,455 @@ class RandomFiniteSetBase(metaclass=abc.ABCMeta):
     def extract_states(self, **kwargs):
         pass
 
+class ProbabilityHypothesisDensity(RandomFiniteSetBase):
+    """ Probability Hypothesis Density Filter
+    
+    
+    """
+    class _TabEntry:
+        def __init__(self):
+            self.label = ()  # time step born, index of birth model born from
+            self.probDensity = None  # must be a distribution class
+            self.meas_assoc_hist = []  # list indices into measurement list per time step
 
+    class _HypothesisHelper:
+        def __init__(self):
+            self.assoc_prob = 0
+            self.track_set = []  # indices in lookup table
+
+        @property
+        def num_tracks(self):
+            return len(self.track_set)
+
+    def __init__(self, **kwargs):
+        self.req_births = 0
+        self.req_surv = 0
+        self.req_upd = 0
+        self.gating_on = False
+        self.inv_chi2_gate = 0
+        self.prune_threshold = 1*10**(-15)
+        self.max_hyps = 3000
+        self.save_covs = False
+
+        self._track_tab = []  # list of all possible tracks
+        self._states = []  # local copy for internal modification
+        self._labels = []  # local copy for internal modification
+        self._meas_tab = []  # list of lists, one per timestep, inner is all meas at time
+        self._meas_asoc_mem = []
+        self._lab_mem = []
+        self._covs = []  # local copy for internal modification
+
+        hyp0 = self._HypothesisHelper()
+        hyp0.assoc_prob = 1
+        hyp0.track_set = []
+        self._hypotheses = [hyp0]  # list of _HypothesisHelper objects
+
+        self._card_dist = []  # probability of having index # as cardinality
+
+        super().__init__(**kwargs)
+
+    @property
+    def states(self):
+        """ Read only list of extracted states.
+
+        This is a list with 1 element per timestep, and each element is a list
+        of the best states extracted at that timestep. The order of each
+        element corresponds to the label order.
+        """
+        return self._states
+
+    @property
+    def labels(self):
+        """ Read only list of extracted labels.
+
+        This is a list with 1 element per timestep, and each element is a list
+        of the best labels extracted at that timestep. The order of each
+        element corresponds to the state order.
+        """
+        return self._labels
+
+    @property
+    def covariances(self):
+        """ Read only list of extracted covariances.
+
+        This is a list with 1 element per timestep, and each element is a list
+        of the best covariances extracted at that timestep. The order of each
+        element corresponds to the state order.
+
+        Raises:
+            RuntimeWarning: If the class is not saving the covariances, and
+                returns an empty list
+        """
+        if not self.save_covs:
+            raise RuntimeWarning("Not saving covariances")
+            return []
+        return self._covs
+    
+    def predict(self, **kwargs):
+        """ Prediction step of the PHD filter.
+
+        This predicts new hypothesis, and propogates them to the next time
+        step. It also updates the cardinality distribution. Because this calls
+        the inner filter's predict function, the keyword arguments must contain
+        any information needed by that function.
+
+        Keyword Args:
+            
+        """
+        # time_step = kwargs['time_step']
+        log_cost = []
+        birth_tab = []
+        for ii, (gm, p) in enumerate(self.birth_terms):
+            cost = p / (1 - p)
+            log_cost.append(-np.log(cost))
+            entry = self._TabEntry()
+            entry.probDensity = deepcopy(gm)
+            # entry.label = (time_step, ii)
+            birth_tab.append(entry)
+
+        # get K best hypothesis, and their index in the lookup table
+        (paths, hyp_cost) = k_shortest(np.array(log_cost), self.req_births)
+
+        # calculate association probabilities for birth hypothesis
+        tot_cost = 0
+        for c in hyp_cost:
+            tot_cost = tot_cost + np.exp(-c).item()
+        birth_hyps = []
+        for (p, c) in zip(paths, hyp_cost):
+            hyp = self._HypothesisHelper()
+            # NOTE: this may suffer from underflow and can be improved
+            hyp.assoc_prob = np.exp(-c).item() / tot_cost
+            hyp.track_set = p
+            birth_hyps.append(hyp)
+
+        # Init and propagate surviving track table
+        surv_tab = []
+        for (ii, track) in enumerate(self._track_tab):
+            gm = self.predict_prob_density(probDensity=track.probDensity,
+                                           **kwargs)
+
+            entry = self._TabEntry()
+            entry.probDensity = gm
+            entry.meas_assoc_hist = deepcopy(track.meas_assoc_hist)
+            # entry.label = track.label
+            surv_tab.append(entry)
+
+        # loop over postierior components
+        surv_hyps = []
+        sum_sqrt_w = 0
+        for hyp in self._hypotheses:
+            sum_sqrt_w = sum_sqrt_w + np.sqrt(hyp.assoc_prob)
+        for hyp in self._hypotheses:
+            if hyp.num_tracks == 0:
+                new_hyp = self._HypothesisHelper()
+                new_hyp.assoc_prob = np.log(hyp.assoc_prob)
+                new_hyp.track_set = hyp.track_set
+                surv_hyps.append(new_hyp)
+            else:
+                cost = self.prob_survive / self.prob_death
+                log_cost = [-np.log(cost)] * hyp.num_tracks
+                k = np.round(self.req_surv * np.sqrt(hyp.assoc_prob)
+                             / sum_sqrt_w)
+                (paths, hyp_cost) = k_shortest(np.array(log_cost), k)
+
+                for (p, c) in zip(paths, hyp_cost):
+                    new_hyp = self._HypothesisHelper()
+                    new_hyp.assoc_prob = hyp.num_tracks \
+                        * np.log(self.prob_death) + np.log(hyp.assoc_prob) \
+                        - c.item()
+                    if len(p) > 0:
+                        new_hyp.track_set = [hyp.track_set[ii] for ii in p]
+                    else:
+                        new_hyp.track_set = []
+                    surv_hyps.append(new_hyp)
+
+        lse = log_sum_exp([x.assoc_prob for x in surv_hyps])
+        for ii in range(0, len(surv_hyps)):
+            surv_hyps[ii].assoc_prob = np.exp(surv_hyps[ii].assoc_prob - lse)
+
+        # Get  predicted hypothesis by convolution
+        self._track_tab = birth_tab + surv_tab
+        self._hypotheses = []
+        tot_w = 0
+        for b_hyp in birth_hyps:
+            for s_hyp in surv_hyps:
+                new_hyp = self._HypothesisHelper()
+                new_hyp.assoc_prob = b_hyp.assoc_prob * s_hyp.assoc_prob
+                tot_w = tot_w + new_hyp.assoc_prob
+                surv_lst = []
+                for x in s_hyp.track_set:
+                    surv_lst.append(x + len(birth_tab))
+                new_hyp.track_set = b_hyp.track_set + surv_lst
+                self._hypotheses.append(new_hyp)
+
+        for ii in range(0, len(self._hypotheses)):
+            self._hypotheses[ii].assoc_prob = (self._hypotheses[ii].assoc_prob
+                                               / tot_w)
+        self._card_dist = self.calc_card_dist(self._hypotheses)
+        self._clean_predictions()
+        
+    def predict_prob_density(self, **kwargs):
+        """ Loops over all elements in a probability distribution and preforms
+        the filter prediction.
+
+        Keyword Args:
+            probDensity (:py:class:`gasur.utilities.distributions.GaussianMixture`): A
+                probability density to run prediction on
+
+        Returns:
+            gm (:py:class:`gasur.utilities.distributions.GaussianMixture`): The
+                predicted probability density
+        """
+        probDensity = kwargs['probDensity']
+        gm_tup = zip(probDensity.means,
+                     probDensity.covariances)
+        c_in = np.zeros((self.filter.get_input_mat().shape[1], 1))
+        gm = GaussianMixture()
+        gm.weights = probDensity.weights.copy()
+        for ii, (m, P) in enumerate(gm_tup):
+            self.filter.cov = P
+            n_mean = self.filter.predict(cur_state=m, cur_input=c_in,
+                                         **kwargs)
+            gm.covariances.append(self.filter.cov.copy())
+            gm.means.append(n_mean)
+
+        return gm
+    
+    def correct(self, **kwargs):
+        """ Correction step of the PHD filter.
+
+        This corrects the hypotheses based on the measurements and gates the
+        measurements according to the class settings. It also updates the
+        cardinality distribution. Because this calls the inner filter's correct
+        function, the keyword arguments must contain any information needed by
+        that function.
+
+        Keyword Args:
+            meas (list): List of Nm x 1 numpy arrays that contain all the
+                measurements needed for this correction
+        """
+        meas = deepcopy(kwargs['meas'])
+        del kwargs['meas']
+        
+    def correct_prob_density(self, meas, **kwargs):
+        """ Loops over all elements in a probability distribution and preforms
+        the filter correction.
+
+        Keyword Args:
+            probDensity (:py:class:`gasur.utilities.distributions.GaussianMixture`): A
+                probability density to run correction on
+            meas (list): List of measurements, each is a N x 1 numpy array
+
+        Returns:
+            tuple containing
+
+                - gm (:py:class:`gasur.utilities.distributions.GaussianMixture`): The
+                  corrected probability density
+                - cost (float): Total cost of for the m best assignment
+        """
+        probDensity = kwargs['probDensity']
+
+        gm = GaussianMixture()
+        for jj in range(0, len(probDensity.means)):
+            self.filter.cov = probDensity.covariances[jj]
+            state = probDensity.means[jj]
+            (mean, qz) = self.filter.correct(meas=meas, cur_state=state,
+                                             **kwargs)
+            cov = self.filter.cov
+            w = qz * probDensity.weights[jj]
+            gm.means.append(mean)
+            gm.covariances.append(cov)
+            gm.weights.append(w)
+        lst = gm.weights
+        lst = [x + np.finfo(float).eps for x in lst]
+        gm.weights = lst
+        cost = sum(gm.weights)
+        for jj in range(0, len(gm.weights)):
+            gm.weights[jj] /= cost
+        return (gm, cost)
+    
+    def extract_states(self, **kwargs):
+        """ Extracts the best state estimates.
+
+        This extracts the best states from the distribution. It should be
+        called once per time step after the correction function. This calls
+        both the inner filters predict and correct functions so the keyword
+        arguments must contain any additional variables needed by those
+        functions.
+
+        Returns:
+            idx_cmp (int): Index of the hypothesis table used when extracting
+                states
+        """
+        
+    def prune(self, **kwargs):
+        """ Removes hypotheses below a threshold.
+
+        This should be called once per time step after the correction and
+        before the state extraction.
+        """
+    
+    def cap(self, **kwargs):
+        """ Removes least likely hypotheses until a maximum number is reached.
+
+        This should be called once per time step after pruning and
+        before the state extraction.
+        """
+    
+    def calc_card_dist(self, hyp_lst):
+        """ Calucaltes the cardinality distribution.
+
+        Args:
+            hyp_lst (list): list of hypotheses to use when finding the
+                distribution
+
+        Returns:
+            (list): Each element is the probability that the index is the
+            cardinality.
+        """
+        
+    def _clean_predictions(self):
+        hash_lst = []
+        for hyp in self._hypotheses:
+            if len(hyp.track_set) == 0:
+                lst = []
+            else:
+                hyp.track_set.sort()
+                lst = [int(x) for x in hyp.track_set]
+            h = hash('*'.join(map(str, lst)))
+            hash_lst.append(h)
+
+        new_hyps = []
+        used_hash = []
+        for ii, h in enumerate(hash_lst):
+            if h not in used_hash:
+                used_hash.append(h)
+                new_hyps.append(self._hypotheses[ii])
+            else:
+                new_ii = used_hash.index(h)
+                new_hyps[new_ii].assoc_prob += self._hypotheses[ii].assoc_prob
+        self._hypotheses = new_hyps
+
+    def _clean_updates(self):
+        used = [0] * len(self._track_tab)
+        for hyp in self._hypotheses:
+            for ii in hyp.track_set:
+                used[ii] += 1
+        nnz_inds = [idx for idx, val in enumerate(used) if val != 0]
+        track_cnt = len(nnz_inds)
+
+        new_inds = [0] * len(self._track_tab)
+        for (ii, v) in zip(nnz_inds, [ii for ii in range(0, track_cnt)]):
+            new_inds[ii] = v
+
+        new_tab = [deepcopy(self._track_tab[ii]) for ii in nnz_inds]
+        new_hyps = []
+        for(ii, hyp) in enumerate(self._hypotheses):
+            if len(hyp.track_set) > 0:
+                hyp.track_set = [new_inds[ii] for ii in hyp.track_set]
+            new_hyps.append(hyp)
+
+        self._track_tab = new_tab
+        self._hypotheses = new_hyps
+
+    def _gate_meas(self, meas, means, covs, **kwargs):
+        if len(meas) == 0:
+            return []
+
+        valid = []
+        for (m, p) in zip(means, covs):
+            meas_mat = self.filter.get_meas_mat(m, **kwargs)
+            est = self.filter.get_est_meas(m, **kwargs)
+            meas_pred_cov = meas_mat @ p @ meas_mat.T + self.filter.meas_noise
+            meas_pred_cov = (meas_pred_cov + meas_pred_cov.T) / 2
+            v_s = cholesky(meas_pred_cov.T)
+            inv_sqrt_m_cov = inv(v_s)
+
+            for (ii, z) in enumerate(meas):
+                if ii in valid:
+                    continue
+                inov = z - est
+                dist = np.sum((inv_sqrt_m_cov.T @ inov)**2)
+                if dist < self.inv_chi2_gate:
+                    valid.append(ii)
+
+        valid.sort()
+        return [meas[ii] for ii in valid]
+    
+    def plot_states_labels(self, plt_inds, **kwargs):
+        """ Plots the best estimate for the states.
+
+        This assumes that the states have been extracted. It's designed to plot
+        two of the state variables (typically x/y position). The error ellipses
+        are calculated according to :cite:`Hoover1984_AlgorithmsforConfidenceCirclesandEllipses`
+
+        Args:
+            plt_inds (list): List of indices in the state vector to plot
+
+        Keyword Args:
+            f_hndl (Matplotlib figure): Current to figure to plot on. Always
+                plots on axes[0], pass None to create a new figure
+            true_states (list): list where each element is a list of numpy
+                N x 1 arrays of each true state. If not given true states
+                are not plotted.
+            sig_bnd (int): If set and the covariances are saved, the sigma
+                bounds are scaled by this number and plotted for each track
+            rng (Generator): A numpy random generator, leave as None for
+                default.
+            meas_inds (list): List of indices in the measurement vector to plot
+                if this is specified all available measurements will be
+                plotted. Note, x-axis is first, then y-axis. Also note, if
+                gating is on then gated measurements will not be plotted.
+            lgnd_loc (string): Location of the legend. Set to none to skip
+                creating a legend.
+
+        Returns:
+            (Matplotlib figure): Instance of the matplotlib figure used
+        """
+
+        f_hndl = kwargs.get('f_hndl', None)
+        true_states = kwargs.get('true_states', None)
+        sig_bnd = kwargs.get('sig_bnd', None)
+        rng = kwargs.get('rng', None)
+        meas_inds = kwargs.get('meas_inds', None)
+        lgnd_loc = kwargs.get('lgnd_loc', None)
+
+        if rng is None:
+            rng = rnd.default_rng(1)
+      
+    def plot_card_dist(self, **kwargs):
+        """ Plots the current cardinality distribution.
+
+        This assumes that the cardinality distribution has been calculated by
+        the class.
+
+        Keyword Args:
+            f_hndl (Matplotlib figure): Current to figure to plot on. Always
+                plots on axes[0], pass None to create a new figure
+
+        Returns:
+            (Matplotlib figure): Instance of the matplotlib figure used
+        """
+
+        f_hndl = kwargs.get('f_hndl', None)
+
+        if len(self._card_dist) == 0:
+            raise RuntimeWarning("Empty Cardinality")
+            return f_hndl
+
+        if f_hndl is None:
+            f_hndl = plt.figure()
+            f_hndl.add_subplot(1, 1, 1)
+
+        x_vals = np.arange(0, len(self._card_dist))
+        f_hndl.axes[0].bar(x_vals, self._card_dist)
+
+        f_hndl.axes[0].set_title("Cardinality Distribution")
+        f_hndl.axes[0].set_ylabel("Probability")
+        f_hndl.axes[0].set_xlabel("Cardinality")
+        plt.tight_layout()
+
+        return f_hndl 
 class GeneralizedLabeledMultiBernoulli(RandomFiniteSetBase):
     """ Delta-Generalized Labeled Multi-Bernoulli filter.
 
